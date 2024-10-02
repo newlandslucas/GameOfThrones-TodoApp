826ef1389b60e0066dc3e7e470c482c401d47d16
newlandslucas <lucasnewlands.m@gmail.com>
2024-10-02 18:27:49 -0300

    feat: refact registrationView | new visual

---
+++ gameOfThrones-TodoList/Core/Authentication/Custom/CustomTextFields.swift
@@ -0,0 +1,96 @@
+//
+//  CustomTextFields.swift
+//  gameOfThrones-TodoList
+//
+//  Created by Lucas Newlands on 02/10/24.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    @Binding var isValid: Bool
+    @Binding var fieldTapped: Bool
+    
+    var body: some View {
+        HStack {
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(Color(.lightGray))
+                        .padding(.horizontal, 2)
+                }
+                
+                TextField("", text: $text)
+                    .foregroundColor(.white)
+                    .font(.subheadline)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 2)
+                    .overlay(Rectangle().frame(height: 1)
+                                .foregroundColor(text.isEmpty ? Color(.darkGray) : .white),
+                             alignment: .bottom)
+                    .onTapGesture {
+                        fieldTapped = true
+                    }
+            }
+            
+            // Ícone de validação
+            if fieldTapped {
+                if isValid && !text.isEmpty {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                } else if !isValid && !text.isEmpty {
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .padding(.horizontal, 10)
+    }
+}
+
+struct CustomSecureField: View {
+    var placeholder: String
+    @Binding var text: String
+    @Binding var isValid: Bool
+    @Binding var fieldTapped: Bool
+    
+    var body: some View {
+        HStack {
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(Color(.lightGray))
+                        .padding(.horizontal, 2)
+                }
+                
+                SecureField("", text: $text)
+                    .foregroundColor(.white)
+                    .font(.subheadline)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 2)
+                    .overlay(Rectangle().frame(height: 1)
+                                .foregroundColor(text.isEmpty ? Color(.darkGray) : .white),
+                             alignment: .bottom)
+                    .onTapGesture {
+                        fieldTapped = true
+                    }
+            }
+            
+            // Ícone de validação
+            if fieldTapped {
+                if text.count >= 8 {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                } else if !text.isEmpty {
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .padding(.horizontal, 10)
+    }
+}
+
+
--- gameOfThrones-TodoList/Core/Authentication/Views/RegistrationView.swift
+++ gameOfThrones-TodoList/Core/Authentication/Views/RegistrationView.swift
@@ -8,15 +8,20 @@
 import SwiftUI
 
 struct RegistrationView: View {
-    @State private var email = ""
-    @State private var password = ""
-    
-    var backgroundColor: Color = Color(UIColor(named: "backgroundColor") ?? .white)
-    
+    @State private var email: String = ""
+    @State private var name: String = ""
+    @State private var password: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showAlert: Bool = false
+    @State private var isEmailValid: Bool = false
+    @State private var isPasswordValid: Bool = false
+    @State private var isNameValid: Bool = false
+    @State private var emailTapped: Bool = false
+    @State private var passwordTapped: Bool = false
+    @State private var nameTapped: Bool = false
+
     var body: some View {
         VStack(spacing: 120) {
-            
-            // MARK: - Title and TextField's
             VStack {
                 HStack {
                     Text("Crie sua\nconta.")
@@ -29,64 +34,78 @@ struct RegistrationView: View {
                 
                 Spacer()
                 
-                VStack(spacing: 10) {
-                    TextField("Insira seu email", text: $email)
-                        .font(.subheadline)
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 10)
+                VStack(spacing: 20) {
+                    // Campo de email com ícone de validação
+                    CustomTextField(placeholder: "Insira seu email", text: $email, isValid: $isEmailValid, fieldTapped: $emailTapped)
+                        .onChange(of: email) { newValue in
+                            isEmailValid = isValidEmail(email)
+                        }
                     
-                    SecureField("Insira sua senha", text: $email)
-                        .font(.subheadline)
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 10)
+                    // Campo de nome com ícone de validação
+                    CustomTextField(placeholder: "Insira seu nome", text: $name, isValid: $isNameValid, fieldTapped: $nameTapped)
+                        .onChange(of: name) { newValue in
+                            isNameValid = !name.isEmpty
+                        }
                     
-                    TextField("Insira seu nome", text: $email)
-                        .font(.subheadline)
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 10)
-                    
-                    TextField("Insira seu username", text: $email)
-                        .font(.subheadline)
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 10)
+                    // Campo de senha com ícone de validação
+                    CustomSecureField(placeholder: "Insira sua senha", text: $password, isValid: $isPasswordValid, fieldTapped: $passwordTapped)
+                        .onChange(of: password) { newValue in
+                            isPasswordValid = password.count >= 8
+                        }
                 }
                 
                 Spacer()
                 
-                Button {
-                    
-                } label: {
-                    Text("Criar conta")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.black)
-                        .frame(width: 350, height: 44)
-                        .background(.white)
-                        .cornerRadius(15)
-                    
-                    
+                if areFieldsFilled() {
+                    Button {
+                        if isEmailValid && isPasswordValid && isNameValid {
+                            print("Conta criada com sucesso!")
+                        } else {
+                            alertMessage = "Verifique se todos os campos estão preenchidos corretamente."
+                            showAlert = true
+                        }
+                    } label: {
+                        Text("Criar conta")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .frame(width: 350, height: 44)
+                            .background(.white)
+                            .cornerRadius(15)
+                    }
+                    .padding(.bottom, 20)
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Erro"),
+                            message: Text(alertMessage),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
                 }
-                .padding(.bottom, 20)
                 
                 Spacer()
             }
             
             Spacer()
-            
         }
         .padding()
         .background(Color.black)
+    }
+    
+    private func areFieldsFilled() -> Bool {
+        return !email.isEmpty && !password.isEmpty && !name.isEmpty
+    }
+    
+    private func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        return emailTest.evaluate(with: email)
     }
 }
 
 #Preview {
     RegistrationView()
 }
+
+
+
