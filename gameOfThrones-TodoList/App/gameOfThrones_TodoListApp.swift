6667c80102e0fba117a80a26274a062b709573bb
newlandslucas <lucasnewlands.m@gmail.com>
2024-11-26 15:56:00 -0300

    feat: homeView

--- gameOfThrones-TodoList/App/gameOfThrones_TodoListApp.swift
+++ gameOfThrones-TodoList/App/gameOfThrones_TodoListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct gameOfThrones_TodoListApp: App {
     var body: some Scene {
         WindowGroup {
-            RegistrationView()
+            HomeView()
         }
     }
 }
--- gameOfThrones-TodoList/Core/Root/ContentView.swift
+++ gameOfThrones-TodoList/Core/Root/ContentView.swift
@@ -16,5 +16,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
